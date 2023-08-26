c82cd6a3fca6add9a98a6aa475ffdf2b69c99586
ZtRXR <Zengtudor@outlook.com>
2023-08-26 16:40:21 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <TopBar></TopBar>
+      </el-header>
+      <el-main style="min-height: 500px">
+        <NuxtPage></NuxtPage>
+      </el-main>
+      <el-footer style="display: flex;justify-content: center">
+        <el-text type="info">
+          网站建设者：曾子愚
+          邮箱：Zengtudor@outlook.com
+          技术栈：Vue3 Vite Nuxt3 ...
+          (本人初中生有意向可以与我讨论)
+        </el-text>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+<script setup lang="ts">
+import TopBar from "~/components/index/TopBar.vue";
+</script>
---
+++ components/index/TopBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+// const handleSelect = (key: string, keyPath: string[]) => {
+//   console.log(key, keyPath)
+// }
+</script>
+
+<template>
+  <el-menu
+      :default-active="'/'"
+      class="el-menu-demo"
+      mode="horizontal"
+      :router="true"
+  >
+    <el-menu-item index="/">ZZIYU DataWeb</el-menu-item>
+    <div class="flex-grow" />
+    <el-sub-menu index="data/all">
+      <template #title>数据收集</template>
+      <el-menu-item index="data/all">总览</el-menu-item>
+      <el-menu-item index="data/bilibili">bilibili</el-menu-item>
+      <el-menu-item index="data/baidu">baidu</el-menu-item>
+      </el-sub-menu>
+    <el-sub-menu index="tools">
+      <template #title>工具</template>
+      <el-menu-item index="tools/qqazk/index">笔顺码字典</el-menu-item>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<style scoped>
+
+</style>
